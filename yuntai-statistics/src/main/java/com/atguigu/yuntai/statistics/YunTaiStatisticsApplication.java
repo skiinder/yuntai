bdf952aacce3de7ff603d6d0e0660a5a5648cf9d
Ruby54 <474894334@qq.com>
2021-12-14 21:18:33 +0800

    first

---
+++ yuntai-statistics/src/main/java/com/atguigu/yuntai/statistics/YunTaiStatisticsApplication.java
@@ -0,0 +1,12 @@
+package com.atguigu.yuntai.statistics;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YunTaiStatisticsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YunTaiStatisticsApplication.class, args);
+    }
+}
