0ccad0b7ed98c4e958a464500d162a3e3e9625f6
Ruby54 <474894334@qq.com>
2021-12-24 08:49:33 +0800

    删除不需要的文件

---
+++ yuntai-access/src/main/java/com/atguigu/yuntai/access/bean/ConnectorBean.java
@@ -0,0 +1,14 @@
+package com.atguigu.yuntai.access.bean;
+
+import com.alibaba.fastjson.JSONObject;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ConnectorBean {
+
+    private String name;
+
+    private String connectorDetail;
+}
